606bef71369302c9c5e4556d30eab9aa06cd5309
Miko Kallas <miko.kallas@gmail.com>
2022-09-19 13:24:37 +0300

    add create a table exercise

---
+++ learn_sql/create_a_table.sql
@@ -0,0 +1,30 @@
+create table friends (
+  id integer,
+  name text,
+  birthday date
+);
+
+insert into friends (id, name, birthday)
+values (1, "Ororo Munroe", "1940-05-30");
+
+insert into friends (id, name, birthday)
+values (2, "Mait Moos", "1989-01-11");
+
+insert into friends (id, name, birthday)
+values (3, "Taavi Tool", "1979-05-27");
+
+update friends
+set name = "Storm"
+where id = 1;
+
+alter table friends
+add column email text;
+
+update friends
+set email = "storm@codecademy.com"
+where id = 1;
+
+delete from friends
+where id = 1;
+
+select * from friends;
